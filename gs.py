47a801bb56eacd4754755c780085f492bcd945a8
Shuo Tang <tangshuo@gmail.com>
2013-05-03 13:45:52 -0500

    profiling citation page

--- gs.py
+++ gs.py
@@ -14,9 +14,6 @@ user = sys.argv[1]
 bib_url = "http://scholar.google.com/citations?view_op=export_citations&hl=en&user=%s" % user
 bib_params = "cit_fmt=0&export_selected_btn=Export+the+article+below&s=%s"
 
-profile_url = "http://scholar.google.com/citations?user=%s&hl=en" % user
-
-
 def get_bibtex(bib_id):
     bib_id = urllib.quote(bib_id)
     bib_req = urllib2.Request(bib_url, headers={'User-Agent' : useragent})
@@ -26,6 +23,15 @@ def get_bibtex(bib_id):
 
 # create a subclass and override the handler methods
 class CitationParser(HTMLParser):
+    cite_url = "http://scholar.google.com/scholar?oi=bibs&hl=en&cites=%s&num=20"
+
+    def __init__(self, cite_id):
+        HTMLParser.__init__(self)
+        req = urllib2.Request(self.cite_url % cite_id, \
+                              headers={'User-Agent' : useragent})
+        url_data = urllib2.urlopen(req).read()
+        self.feed(url_data)
+
     def handle_starttag(self, tag, attrs):
         for attr in attrs:
             if len(attr) >= 2 and attr[0] == "onclick" and \
@@ -39,19 +45,30 @@ class CitationParser(HTMLParser):
         pass
 
 class ProfileParser(HTMLParser):
+    profile_url = "http://scholar.google.com/citations?user=%s&hl=en"
     citation_for_view = "citation_for_view="
+
+    def __init__(self, profile_id):
+        HTMLParser.__init__(self)
+        req = urllib2.Request(self.profile_url % profile_id, \
+                              headers={'User-Agent' : useragent})
+        url_data = urllib2.urlopen(req).read()
+        self.feed(url_data)
+
 
     def handle_starttag(self, tag, attrs):
         for attr in attrs:
-            if len(attr) >= 2 and attr[0] == "href" and \
-               attr[1].startswith("/citations?view_op=view_citation"):
-                idx = attr[1].find(self.citation_for_view) + \
-                    len(self.citation_for_view)
-                bib_id = attr[1][idx:]
-                print get_bibtex(bib_id)
+            if len(attr) >= 2 and attr[0] == "href":
+                if attr[1].startswith("/citations?view_op=view_citation"):
+                    idx = attr[1].find(self.citation_for_view) + \
+                          len(self.citation_for_view)
+                    bib_id = attr[1][idx:]
+                    print get_bibtex(bib_id)
+                elif attr[1].find("/scholar?oi=bibs&hl=en&") > 0:
+                    idx = attr[1].find("cites=") + len("cites=")
+                    cite_id = attr[1][idx:]
+                    CitationParser(cite_id)
+
 
 # instantiate the parser and fed it some HTML
-profile = ProfileParser()
-req = urllib2.Request(profile_url, headers={'User-Agent' : useragent})
-url_data = urllib2.urlopen(req).read()
-profile.feed(url_data)
+ProfileParser(user)
