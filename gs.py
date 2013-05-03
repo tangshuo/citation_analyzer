0d5a1ffd2b62250121295db4c9299181e90d8cdf
Shuo Tang <tangshuo@gmail.com>
2013-05-03 17:05:08 -0500

    get all bibtex this time really

--- gs.py
+++ gs.py
@@ -3,6 +3,9 @@
 import sys
 import urllib
 import urllib2
+import hashlib
+import random
+import glob
 
 from HTMLParser import HTMLParser
 
@@ -12,7 +15,9 @@ useragent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko)
 user = sys.argv[1]
 
 bib_url = "http://scholar.google.com/citations?view_op=export_citations&hl=en&user=%s" % user
+bib_id_url = "http://scholar.google.com/scholar.bib?q=info:%s:scholar.google.com/&output=citation&hl=en"
 bib_params = "cit_fmt=0&export_selected_btn=Export+the+article+below&s=%s"
+google_id = hashlib.md5(str(random.random())).hexdigest()[:16]
 
 def get_bibtex(bib_id):
     bib_id = urllib.quote(bib_id)
@@ -22,7 +27,15 @@ def get_bibtex(bib_id):
     return bib_data
 
 def get_bibtex_by_id(bib_id):
-    return get_bibtex("%s:%s" % (user, bib_id))
+    bib_req = urllib2.Request(bib_id_url % bib_id, headers={'User-Agent' : useragent, 'Cookie' : 'GSP=ID=%s:CF=4' % google_id})
+    bib_data = urllib2.urlopen(bib_req).read()
+
+    return bib_data
+
+def analyse(filename):
+    bib_file = open(filename, 'r')
+    print bib_file.read()
+    bib_file.close()
 
 class CitationSubPageParser(HTMLParser):
     bib_list = []
@@ -78,13 +91,16 @@ class CitationParser(HTMLParser):
 class ProfileParser(HTMLParser):
     profile_url = "http://scholar.google.com/citations?user=%s&hl=en"
     citation_for_view = "citation_for_view="
+    bib_file = None
 
     def __init__(self, profile_id):
         HTMLParser.__init__(self)
+        self.bib_file = None
         req = urllib2.Request(self.profile_url % profile_id, \
                               headers={'User-Agent' : useragent})
         url_data = urllib2.urlopen(req).read()
         self.feed(url_data)
+        self.calculate_citation()
 
     def handle_starttag(self, tag, attrs):
         for attr in attrs:
@@ -93,14 +109,23 @@ class ProfileParser(HTMLParser):
                     idx = attr[1].find(self.citation_for_view) + \
                           len(self.citation_for_view)
                     bib_id = attr[1][idx:]
-                    print get_bibtex(bib_id)
+                    self.calculate_citation()
+                    self.bib_file = open("%s.bib" % bib_id.replace(':', '_'), 'w')
+                    self.bib_file.write(get_bibtex(bib_id))
                 elif attr[1].find("/scholar?oi=bibs&hl=en&") > 0:
                     idx = attr[1].find("cites=") + len("cites=")
                     cite_id = attr[1][idx:]
                     c_parser = CitationParser(cite_id)
                     for citation in  c_parser.get_all_citations():
-                        print get_bibtex_by_id(citation)
+                        self.bib_file.write(get_bibtex_by_id(citation))
 
+    def calculate_citation(self):
+        if self.bib_file is not None:
+            filename = self.bib_file.name
+            self.bib_file.close()
+            analyse(filename)
 
 # instantiate the parser and fed it some HTML
-ProfileParser(user)
+#ProfileParser(user)
+for filename in glob.glob("*.bib"):
+    analyse(filename)
